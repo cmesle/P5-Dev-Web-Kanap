bc7e7bb1d5ea2e75c5e185b6c7d6c747a0891922
cmesle <cecile.mesle@gmail.com>
2022-06-10 20:08:53 +0200

    nettoyage

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,4 @@
 const pageCourante = document.location.href;
-
-// let _id = pageCourante.substring(pageCourante.lastIndexOf('=')+1);
 let url = new URL(pageCourante)
 let orderID = url.searchParams.get('orderID');
 
