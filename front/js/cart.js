f448d3362501cbe6083d0dfda7d66425ab35b18e
cmesle <cecile.mesle@gmail.com>
2022-06-14 10:34:46 +0200

    mise en forme + ajout fonction formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,14 +9,14 @@ var totalPrice = document.getElementById('totalPrice')
 let cart = JSON.parse(localStorage.getItem("articleLS"))//  récupération du localStorage
 
 fetch('http://localhost:3000/api/products')
-.then(res=>res.json())
-.then(data => {afficherCommande(data)})
+  .then(res => res.json())
+  .then(data => { afficherCommande(data) })
 
 function afficherCommande(tableauCanapes) {
   tableauCanapes.forEach(canape => {
-    for (i=0; i < cart.length; i++) {
-      if (canape._id==cart[i][0]) {
-        let affichage = 
+    for (i = 0; i < cart.length; i++) {
+      if (canape._id == cart[i][0]) {
+        let affichage =
           `<article class="cart__item" data-id="${cart[i][0]}" data-color="${cart[i][1]}">
             <div class="cart__item__img">
               <img src="${canape.imageUrl}" alt="${canape.altTxt}">
@@ -46,33 +46,33 @@ function afficherCommande(tableauCanapes) {
       }
     }
   })
-  }
+}
 
 //  AFFICHE LE NOMBRE D'ARTICLES DANS LA COMMANDE
-function quantiteTotale (nombre) {
+function quantiteTotale(nombre) {
   nbArticle += parseInt(nombre, 10)
   totalQuantity.textContent = nbArticle
 }
 
 //  CALCULE ET AFFICHE LE MONTANT TOTAL DE LA COMMANDE
 function prixTotal(prix, nombre) {
-  montantTotal += prix*nombre
+  montantTotal += prix * nombre
   totalPrice.textContent = montantTotal
 }
 
 //  SUPPRIME L'ARTICLE DU PANIER ET RECHARGE LA PAGE
 function supprimerArticle(currentArticleID, currentArticleColor) {
-  let i=0
+  let i = 0
   let suppression = false
-  while(i<cart.length && !suppression) {
-    if(cart[i][0]==currentArticleID && cart[i][1]==currentArticleColor) {
+  while (i < cart.length && !suppression) {
+    if (cart[i][0] == currentArticleID && cart[i][1] == currentArticleColor) {
       cart.splice(i, 1)
       suppression = true
     }
     i++
   }
 
-  if (cart.length==0) {
+  if (cart.length == 0) {
     localStorage.clear()
     window.location = 'cart.html'
   } else {
@@ -82,24 +82,24 @@ function supprimerArticle(currentArticleID, currentArticleColor) {
 }
 
 //  ajout d'un délai pour garantir que le DOM est construit avant de pouvoir en sélectionner les éléments .deleteItem et .itemQuantity
-  setTimeout(btnActivation, 500)
-  setTimeout(inputActivation, 500)
+setTimeout(btnActivation, 500)
+setTimeout(inputActivation, 500)
 //  ACTIVE LES BOUTONS SUPPRIMER (.deleteItem)
 function btnActivation() {
   var supprBtns = document.getElementsByClassName('deleteItem')
-    for (i=0; i<supprBtns.length; i++) {
-      let btn = supprBtns[i]
-        btn.addEventListener('click', function(e) {
-          e.preventDefault()
-          if (confirm('Etes vous sûr de vouloir supprimer cet article ?')) {
-            let currentArticle = btn.closest('article')  //  sélectionne l'article, trouve son index
-            let currentArticleID = currentArticle.dataset['id']
-            let currentArticleColor = currentArticle.dataset['color']
-            supprimerArticle(currentArticleID, currentArticleColor)
-          }
-        })
-    }
+  for (i = 0; i < supprBtns.length; i++) {
+    let btn = supprBtns[i]
+    btn.addEventListener('click', function (e) {
+      e.preventDefault()
+      if (confirm('Etes vous sûr de vouloir supprimer cet article ?')) {
+        let currentArticle = btn.closest('article')  //  sélectionne l'article, trouve son index
+        let currentArticleID = currentArticle.dataset['id']
+        let currentArticleColor = currentArticle.dataset['color']
+        supprimerArticle(currentArticleID, currentArticleColor)
+      }
+    })
   }
+}
 
 
 //  ----------  MISE A JOUR QUANTITE ET PRIX TOTAUX ------------------
@@ -107,24 +107,24 @@ function btnActivation() {
 var qteArticle = document.getElementsByClassName('itemQuantity')
 
 //  ACTIVE LES INPUT QUANTITE  (.itemQuantity)
-function inputActivation() { 
-  for (i=0 ; i < qteArticle.length ; i++) {       //  boucle qui sélectionne les input et leur ajoute un eventListener
+function inputActivation() {
+  for (i = 0; i < qteArticle.length; i++) {       //  boucle qui sélectionne les input et leur ajoute un eventListener
     let qte = qteArticle[i]
     let qteInitiale = parseInt(qte.value)
 
     let currentArticle = qte.closest('article')  //  sélectionne l'article
     let _id = currentArticle.dataset['id']
-    
+
     fetch(`http://localhost:3000/api/products/${_id}`)
-    .then(res=> res.json())
-    .then(data => getPrice(data))
+      .then(res => res.json())
+      .then(data => getPrice(data))
 
     let prixCanape
     function getPrice(canape) {
       prixCanape = canape.price
     }
 
-    qte.addEventListener('change', function(e) {
+    qte.addEventListener('change', function (e) {
       e.preventDefault()
       e.stopPropagation()
       let currentArticleID = currentArticle.dataset['id']
@@ -137,10 +137,10 @@ function inputActivation() {
         let difference = newItemQuantity - qteInitiale
         qteInitiale = qte.value                            //  réinitialisation de qteInitiale pour changements ultérieurs
         quantiteTotale(difference)                         // mise à jour du nombre total d'articles
-                                                           // mise à jour du localStorage
-        let ligneAModifier = (cartItem) => cartItem[0]==currentArticleID && cartItem[1]==currentArticleColor
+        // mise à jour du localStorage
+        let ligneAModifier = (cartItem) => cartItem[0] == currentArticleID && cartItem[1] == currentArticleColor
         cart[cart.findIndex(ligneAModifier)][2] += difference
-        localStorage.setItem('articleLS', JSON.stringify(cart)) 
+        localStorage.setItem('articleLS', JSON.stringify(cart))
 
         prixTotal(prixCanape, difference)                  // mise à jour du montant total de la commande
       }
@@ -149,43 +149,155 @@ function inputActivation() {
 }
 
 
-//  ----------  CREATION DU TABLEAU A ENVOYER A L'API ------------------
 
-let products = JSON.parse(localStorage.getItem("articleLS"))
 
-products.forEach(cartItem => {
-  cartItem.pop()
-  cartItem.pop()
-})
+function formulaire() {
 
-// mise au format des éléments du tableau products (de tableau de tableaux au format attendu : tableau de strings )
-for (i=0; i<products.length; i++) {
-  products[i] = products[i].toString()
+  const prenom = document.getElementById('firstName')
+  const nom = document.getElementById('lastName')
+  const adresse = document.getElementById('address')
+  const ville = document.getElementById('city')
+  const email = document.getElementById('email')
+  const commander = document.getElementById('order')
+
+  const erreurPrenom = document.getElementById('firstNameErrorMsg')
+  const erreurNom = document.getElementById('lastNameErrorMsg')
+  const erreurAdresse = document.getElementById('addressErrorMsg')
+  const erreurVille = document.getElementById('cityErrorMsg')
+  const erreurEmail = document.getElementById('emailErrorMsg')
+
+
+  const erreurPrenomMsg = 'Votre prénom avec que des lettres svp'
+  const erreurNomMsg = 'Votre nom avec que des lettres svp'
+  const erreurAdresseMsg = 'Votre adresse ?'
+  const erreurVilleMsg = 'Votre ville ?'
+  const erreurEmailMsg = 'format attendu : exemple@exemple.ex'
+
+  const textOnlyRegex = /^[A-Z][A-Za-zÀ-ÖØ-öø-ÿ' -]+/
+  const numbersOnlyRegex = /[\D]/
+
+  // vidage des champs au reload
+  // prenom.value =''
+  // nom.value =''
+  // adresse.value = ''
+  // ville.value = ''
+  // email.value = ''
+
+  //  ----------  VALIDATION DU FORMAT DES SAISIES UTILISATEURS DANS LES CHAMPS DU FORMULAIRE ------------------
+
+  // vérification du format des champs sans nombre firstName, lastName, city
+  prenom.addEventListener('input', function (e) {
+    e.preventDefault
+    let value = e.target.value;
+    let test = textOnlyRegex.test(value)
+    if (!test) {
+      erreurPrenom.textContent = erreurPrenomMsg
+      prenom.focus()
+    } else {
+      erreurPrenom.textContent = ''
+    }
+  });
+
+  nom.addEventListener('blur', function (e) {
+    let value = e.target.value;
+    let test = textOnlyRegex.test(value)
+    if (test) {
+      erreurNom.textContent = ''
+    } else {
+      erreurNom.textContent = erreurNomMsg
+    }
+  });
+
+  ville.addEventListener('blur', function (e) {
+    let value = e.target.value;
+    let test = textOnlyRegex.test(value)
+    if (test) {
+      erreurVille.textContent = ''
+      isValid = true;
+    } else {
+      isValid = false;
+      erreurVille.textContent = erreurVilleMsg
+    }
+  });
+
+  // vérification du champ adresse (ne peut pas contenir que des nombres)
+  adresse.addEventListener('blur', function (e) {
+    let value = e.target.value;
+    let test = numbersOnlyRegex.test(value)
+    if (test) {
+      erreurAdresse.textContent = ''
+      // isValid = true;
+    } else {
+      // isValid = false;
+      erreurAdresse.textContent = erreurAdresseMsg
+    }
+  });
+
+  // vérification du format du champs email
+  email.addEventListener('change', function (e) {
+    var value = e.target.value;
+    var emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    var test = emailRegEx.test(value)
+    if (!test) {
+      erreurEmail.textContent = erreurEmailMsg
+    }
+  })
+
+
+  //  ----------  BOUTON "COMMANDER" : VALIDATION DU FORMULAIRE ET ENVOI DES DONNEES A L'API  ------------------
+
+  commander.addEventListener('click', function (e) {
+    e.preventDefault()
+    var contact = {
+      firstName: prenom.value,
+      lastName: nom.value,
+      address: adresse.value,
+      city: ville.value,
+      email: email.value
+    }
+    if (prenom.value == '' || nom.value == '' || adresse.value == '' || ville.value == '' || email.value == '') {
+      alert('tous les champs sont obligatoires')
+    } else {
+      var contact = {
+        firstName: prenom.value,
+        lastName: nom.value,
+        address: adresse.value,
+        city: ville.value,
+        email: email.value
+      }
+
+      //  ----------  CREATION DU TABLEAU A ENVOYER A L'API ------------------
+
+      let products = JSON.parse(localStorage.getItem("articleLS"))
+
+      products.forEach(cartItem => {
+        cartItem.pop()
+        cartItem.pop()
+      })
+
+      // mise au format des éléments du tableau products (de tableau de tableaux au format attendu : tableau de strings )
+      for (i = 0; i < products.length; i++) {
+        products[i] = products[i].toString()
+      }
+      // envoi des données à l'API et redirection vers la page confirmation
+      let commande = { contact, products }                      //  création de l'objet à envoyer
+
+      fetch('http://localhost:3000/api/products/order', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(commande)
+      })
+        .then(async (response) => {                             //  redirection vers la page confirmation avec orderId
+          let orderID = await response.json()
+          let param = orderID.orderId
+          window.location = 'confirmation.html?orderID=' + param
+        })
+    }
+
+  })
+
 }
 
-// var goOn
-//  function warning(message) {
-//   let popUp = document.createElement('div')
-//   document.body.appendChild(popUp)
-//   popUp.style = ` position: absolute;
-//                   left: 25%;
-//                   top: 40%;
-//                   width: 50%;
-//                   padding: 20px;
-//                   background: rgb(44, 62, 80);
-//                   border-radius: 20px;
-//                   text-align: center;`
-//   popUp.innerHTML = `<p>${message}</p>
-//                     <button id='confirmer'>confirmer</button>
-//                     <button id='annuler'>annuler</button>`
-//   let confirmer = document.getElementById('confirmer')
-//   confirmer.addEventListener('click', function(e) {
-//     e.preventDefault()
-//     goOn = true
-//   })
-//   let annuler = document.getElementById('annuler')
-//   annuler.addEventListener('click', function(e) {
-//     e.preventDefault()
-//     goOn = false
-//   })
-// }
