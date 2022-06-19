ea52f831762c59efdd000a9843d9588c77c28bf0
cmesle <cecile.mesle@gmail.com>
2022-06-19 18:15:24 +0200

    ajout verif formulaire dans commander ; suppression var contact en double dans commander

--- front/js/cart.js
+++ front/js/cart.js
@@ -46,6 +46,7 @@ function afficherCommande(tableauCanapes) {
       }
     }
   })
+  formulaire()
 }
 
 //  AFFICHE LE NOMBRE D'ARTICLES DANS LA COMMANDE
@@ -173,74 +174,82 @@ function formulaire() {
   const erreurVilleMsg = 'Votre ville ?'
   const erreurEmailMsg = 'format attendu : exemple@exemple.ex'
 
-  const textOnlyRegex = /^[A-Z][A-Za-zÀ-ÖØ-öø-ÿ' -]+/
-  const numbersOnlyRegex = /[\D]/
+  const textOnlyRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ, '-]+$/     //(/^[A-Za-z]+$/)
+  const adressRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9, '-]+$/
+  const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
-  // vidage des champs au reload
-  // prenom.value =''
-  // nom.value =''
-  // adresse.value = ''
-  // ville.value = ''
-  // email.value = ''
 
   //  ----------  VALIDATION DU FORMAT DES SAISIES UTILISATEURS DANS LES CHAMPS DU FORMULAIRE ------------------
-
+  let prenomOK = false
+  let nomOK = false
+  let adresseOK = false
+  let villeOK = false
+  let emailOK = false
   // vérification du format des champs sans nombre firstName, lastName, city
-  prenom.addEventListener('input', function (e) {
+  prenom.addEventListener('blur', function (e) {
     e.preventDefault
     let value = e.target.value;
     let test = textOnlyRegex.test(value)
-    if (!test) {
-      erreurPrenom.textContent = erreurPrenomMsg
-      prenom.focus()
-    } else {
+    if (test) {
       erreurPrenom.textContent = ''
+      prenomOK = true
+      console.log(prenomOK)
+    } else {
+      erreurPrenom.textContent = erreurPrenomMsg
+      prenomOK = false
     }
-  });
+  })
 
-  nom.addEventListener('blur', function (e) {
+  nom.addEventListener('change', function (e) {
     let value = e.target.value;
     let test = textOnlyRegex.test(value)
     if (test) {
       erreurNom.textContent = ''
+      nomOK = true
     } else {
       erreurNom.textContent = erreurNomMsg
+      nomOK = false
     }
-  });
+  })
 
-  ville.addEventListener('blur', function (e) {
+  ville.addEventListener('input', function (e) {
     let value = e.target.value;
     let test = textOnlyRegex.test(value)
     if (test) {
       erreurVille.textContent = ''
-      isValid = true;
+      villeOK = true
     } else {
-      isValid = false;
       erreurVille.textContent = erreurVilleMsg
+      villeOK = false
     }
-  });
+  })
 
   // vérification du champ adresse (ne peut pas contenir que des nombres)
-  adresse.addEventListener('blur', function (e) {
+  adresse.addEventListener('input', function (e) {
     let value = e.target.value;
-    let test = numbersOnlyRegex.test(value)
+    let test = adressRegex.test(value)
     if (test) {
       erreurAdresse.textContent = ''
-      // isValid = true;
+      adresseOK = true
     } else {
-      // isValid = false;
       erreurAdresse.textContent = erreurAdresseMsg
+      adresseOK = false
     }
   });
 
   // vérification du format du champs email
-  email.addEventListener('change', function (e) {
-    var value = e.target.value;
-    var emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-    var test = emailRegEx.test(value)
-    if (!test) {
+  email.addEventListener('input', function (e) {
+    let value = e.target.value;
+    let test = emailRegEx.test(value)
+    if (test) {
+      erreurEmail.textContent = ''
+      emailOK = true
+
+    } else {
       erreurEmail.textContent = erreurEmailMsg
+      emailOK = false
     }
+    console.log(emailOK)
   })
 
 
@@ -248,15 +257,11 @@ function formulaire() {
 
   commander.addEventListener('click', function (e) {
     e.preventDefault()
-    var contact = {
-      firstName: prenom.value,
-      lastName: nom.value,
-      address: adresse.value,
-      city: ville.value,
-      email: email.value
-    }
+
     if (prenom.value == '' || nom.value == '' || adresse.value == '' || ville.value == '' || email.value == '') {
-      alert('tous les champs sont obligatoires')
+      alert('tous les champs doivent être remplis avant de passer votre commande')
+    } else if (!prenomOK || !nomOK || !adresseOK || !villeOK || !emailOK) {
+      alert('certains des champs sont mal remplis')
     } else {
       var contact = {
         firstName: prenom.value,
