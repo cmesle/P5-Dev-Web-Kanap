fc9a8939d6e58a3c5f3d0b97774576602e7a9d60
cmesle <cecile.mesle@gmail.com>
2022-06-25 17:58:48 +0200

    mise en forme

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,9 +1,12 @@
 confirmationCommande()
 
+// AFFICHE L'IDENTIFIANT DE COMMANDE DANS LA PAGE
 function confirmationCommande() {
-    const pageCourante = document.location.href;
+    // récupère l'identifiant de commande en paramètre de l'URL
+    const pageCourante = document.location.href
     let url = new URL(pageCourante)
-    let orderID = url.searchParams.get('orderID');
+    let orderID = url.searchParams.get('orderID')
 
+    // affiche l'identifiant de commande dans la page
     document.getElementById('orderId').textContent = orderID
 }
